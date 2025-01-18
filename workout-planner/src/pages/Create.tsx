7d3a8d15f702477113d12971f2a0a5255627e658
sanamohanna <sanamohanna2305@gmail.com>
2025-01-18 15:51:24 +0200

    100% finished

--- workout-planner/src/pages/Create.tsx
+++ workout-planner/src/pages/Create.tsx
@@ -12,14 +12,22 @@ import {
   IonIcon,
   IonImg,
 } from '@ionic/react';
-import { addCircleOutline, trashOutline, checkmarkCircleOutline } from 'ionicons/icons';
+import {
+  closeOutline,
+  checkmarkOutline,
+  chevronDownOutline,
+  chevronUpOutline,
+  addCircleOutline,
+  trashOutline,
+  checkmarkCircleOutline,
+} from 'ionicons/icons'; // Import the icons
 import './Create.css';
 import exercisesData from '../Data/Exercises.json';
-import backgroundImg from '../imgs/plan.png';
 const Create: React.FC = () => {
   const [showLibrary, setShowLibrary] = useState(false);
   const [selectedExercises, setSelectedExercises] = useState<{ [muscle: string]: any[] }>({});
   const [lastModified, setLastModified] = useState<number | null>(null);
+  const [expandedMuscles, setExpandedMuscles] = useState<string[]>([]);
 
   // Group exercises by muscle
   const muscleGroups = exercisesData.reduce((acc: { [muscle: string]: any[] }, exercise) => {
@@ -104,6 +112,15 @@ const Create: React.FC = () => {
     }));
   };
 
+   // Function to toggle muscle group visibility
+   const toggleMuscleGroup = (muscle: string) => {
+    setExpandedMuscles((prev) =>
+      prev.includes(muscle)
+        ? prev.filter((m) => m !== muscle) // Remove muscle from expanded
+        : [...prev, muscle] // Add muscle to expanded
+        
+    );
+  };
   // Clear all exercises and remove from localStorage
   const clearExercises = () => {
     setSelectedExercises({});
@@ -132,62 +149,87 @@ const Create: React.FC = () => {
           </div>
         )}
 
-        {/* Library View */}
-        {showLibrary && (
-          <div className="library-view">
-            <IonButton
-              expand="block"
-              color="medium"
-              onClick={() => setShowLibrary(false)}
-              className="cancel-button">
-              Cancel
-            </IonButton>
-            <h2>Select Exercises</h2>
-            {Object.keys(muscleGroups).map((muscle) => (
-              <div key={muscle} className="muscle-group">
-                <h3>{muscle}</h3>
-                {muscleGroups[muscle].map((exercise) => (
-                  <IonItem key={exercise.id} lines="full">
-                    <IonImg
-                      slot="start"
-                      src={`src/Data/exercises_gif/${exercise.images}`}
-                      className="exercise-img"
-                      alt={exercise.name}
-                    />
-                    <IonLabel>
-                      <h2>{exercise.name}</h2>
-                      <p>{exercise.category}</p>
-                    </IonLabel>
-                    {!selectedExercises[muscle]?.find((ex) => ex.id === exercise.id) && (
-                      <IonIcon
-                        icon={addCircleOutline}
-                        slot="end"
-                        size="large"
-                        color="primary"
-                        style={{ cursor: 'pointer' }}
-                        onClick={() => selectExercise(exercise)}
-                      />
-                    )}
-                    {selectedExercises[muscle]?.find((ex) => ex.id === exercise.id) && (
-                      <IonIcon
-                        icon={trashOutline}
-                        slot="end"
-                        size="large"
-                        color="danger"
-                        style={{ cursor: 'pointer' }}
-                        onClick={() => deleteExercise(muscle, exercise.id)}
-                      />
-                    )}
-                  </IonItem>
-                ))}
-              </div>
-            ))}
-            <IonButton expand="block" color="primary" onClick={() => setShowLibrary(false)}>
-              Done Selecting
-            </IonButton>
-          </div>
-        )}
+          {/* Library View*/}
+          {showLibrary && (
+            <div className="library-view">
+              <div className="icon-buttons-container">
+                {/* Cancel Button */}
+                <button
+                  className="cancel-icon-button"
+                  onClick={() => setShowLibrary(false)} // Logic to close the library view
+                >
+                  <IonIcon icon={closeOutline} />
+                </button>
 
+                {/* Check Button */}
+                <button
+                  className="check-icon-button"
+                  onClick={() => {
+                    setShowLibrary(false); // Logic to close the library view
+                  }}
+                >
+                  <IonIcon icon={checkmarkOutline} />
+                </button>
+              </div>
+              <h2>Select Exercises</h2>
+              {Object.keys(muscleGroups).map((muscle) => (
+                <div key={muscle} className="muscle-group">
+                  {/* Muscle Group Title */}
+                  <IonItem button onClick={() => toggleMuscleGroup(muscle)} lines="full" key={muscle} detail={false}>
+                  <IonLabel>{muscle}</IonLabel>
+                
+                  <IonIcon
+                    icon={expandedMuscles.includes(muscle) ? chevronUpOutline : chevronDownOutline}
+                    slot="end"
+                  />
+                </IonItem>
+
+                  {/* Exercise List - Shown only if expanded */}
+                  {expandedMuscles.includes(muscle) && (
+                    <IonList>
+                      {muscleGroups[muscle].map((exercise) => (
+                        <IonItem key={exercise.id} lines="full">
+                          <IonImg
+                            slot="start"
+                            src={`src/Data/exercises_gif/${exercise.images}`}
+                            className="exercise-img"
+                            alt={exercise.name}
+                          />
+                          <IonLabel>
+                            <h2>{exercise.name}</h2>
+                            <p>{exercise.category}</p>
+                          </IonLabel>
+                          {!selectedExercises[muscle]?.find((ex) => ex.id === exercise.id) && (
+                            <IonIcon
+                              icon={addCircleOutline}
+                              slot="end"
+                              size="large"
+                              color="primary"
+                              style={{ cursor: 'pointer' }}
+                              onClick={() => selectExercise(exercise)}
+                            />
+                          )}
+                          {selectedExercises[muscle]?.find((ex) => ex.id === exercise.id) && (
+                            <IonIcon
+                              icon={trashOutline}
+                              slot="end"
+                              size="large"
+                              color="danger"
+                              style={{ cursor: 'pointer' }}
+                              onClick={() => deleteExercise(muscle, exercise.id)}
+                            />
+                          )}
+                        </IonItem>
+                      ))}
+                    </IonList>
+                  )}
+                </div>
+              ))}
+              <IonButton expand="block" color="primary" onClick={() => setShowLibrary(false)}>
+                Done Selecting
+              </IonButton>
+            </div>
+          )}
         {/* Selected Exercises */}
         {!showLibrary && Object.keys(selectedExercises).length > 0 && (
           <div className="selected-exercises">
